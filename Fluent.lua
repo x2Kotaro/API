e326f82d13cea9be2e2b7fe6da38413b2c6c0acd
ElevatorDark HUB <93924562+x2Kotaro@users.noreply.github.com>
2025-04-29 20:32:22 +0700

    Update Fluent.lua

--- Fluent.lua
+++ Fluent.lua
@@ -5076,7 +5076,7 @@ local aa = {
     end,
     [47] = function()
         local aa, ab, ac, ad, ae = b(47)
-        local af = {Names = {"Dark", "Darker", "Light", "Aqua", "Amethyst", "Rose"}}
+        local af = {Names = {"Dark", "Darker", "Light", "Aqua", "Amethyst", "Rose", "Venuz"}}
         for ag, ah in next, ab:GetChildren() do
             local aj = ac(ah)
             af[aj.Name] = aj
