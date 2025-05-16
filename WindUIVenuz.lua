9bcebc408cbaeb691faead8194296255f81a1942
ElevatorDark HUB <93924562+x2Kotaro@users.noreply.github.com>
2025-05-16 17:21:24 +0700

    Update WindUIVenuz.lua

--- WindUIVenuz.lua
+++ WindUIVenuz.lua
@@ -4,7 +4,7 @@
     | |/ |/ / / _ \/ _  / /_/ // /  
     |__/|__/_/_//_/\_,_/\____/___/
     
-    This script is modified by Phoenix Version 0.0.1
+    This script is modified by Phoenix Version 0.0.1111111111
 ]]
 
 
@@ -158,7 +158,7 @@ do
                     end 
                     
                 function g.NewRoundFrame(h,i,j,k,n)
-                    local o = g.New(n and 'ImageButton' or 'ImageLabel',{Image = i == 'SquircleOutline' and 'rbxassetid:// 117788349049947' or  i=='Shadow-sm' and 'rbxassetid://84825982946844' or i == 'Squircle-TL-TR' and 'rbxassetid://73569156276236',ScaleType = 'Slice',
+                    local o = g.New(n and 'ImageButton' or 'ImageLabel',{Image = i == 'Squircle' and 'rbxassetid://80999662900595' or  i=='SquircleOutline' and 'rbxassetid:// 117788349049947' or  i=='Shadow-sm' and 'rbxassetid://84825982946844' or i == 'Squircle-TL-TR' and 'rbxassetid://73569156276236',ScaleType = 'Slice',
                     SliceCenter = i ~= 'Shadow-sm' and Rect.new(256,256,256,256) or Rect.new(512,512,512,512),
                     SliceScale = 1,
                     BackgroundTransparency = 1,
