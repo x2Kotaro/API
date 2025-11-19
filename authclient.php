e5b0b5143b1abe4154eb21451f46b3aae7d86f37
Venuz Hub <93924562+x2Kotaro@users.noreply.github.com>
2025-11-19 19:21:23 +0700

    Update authclient.php

--- authclient.php
+++ authclient.php
@@ -4,17 +4,18 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
-$servername = "localhost";
+$servername = "caboose.proxy.rlwy.net";
 $username = "root";
-$password = "";
-$dbname = "roblox_auth";
+$password = "uuEilzwNfhvWKZaCEOcIdDSRIHyChOZb";
+$dbname = "railway";
+$port = 39358;
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die(json_encode([
         'success' => false,
-        'message' => 'Database connection failed'
+        'message' => 'Database connection failed: ' . $conn->connect_error
     ]));
 }
 
